43d48d693da7d95e199bd17a8301ff09bf9e906a
Entouanes <coma@NB29629.elcaNet.local>
2023-02-15 17:31:17 +0100

    Nice job

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,4 @@
+import Apexchart from "./Apexchart";
 import Timeline from "./Timeline";
 
 export default function Card() {
@@ -5,12 +6,11 @@ export default function Card() {
   return (  
       <div className="m-10 bg-white rounded-lg p-4 ring-1 ring-slate-900/5 shadow-xl">
         <div>
-          <span className="inline-flex items-center justify-center p-2 bg-white rounded-md text-indigo-500 ring-1 ring-indigo-500">
+          <span className="inline-flex items-center justify-center p-2 m-2 bg-white rounded-md text-indigo-500 ring-1 ring-indigo-500">
             Run ID: 1
           </span>
         </div>
-        <h3 className="text-slate-900 mt-5 text-base font-medium tracking-tight">Execution timeline</h3>
-  
+        <h3 className="text-slate-900 mt-5 p-2 text-base font-medium tracking-tight">Run timeline (microseconds)</h3>
         <div className="pl-4 pr-4">
           <Timeline/>
         </div>
--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,72 +1,252 @@
-import Chart from "react-apexcharts"
+import ReactApexChart from "react-apexcharts"
+
+const indigo = '#16a34a';
+const red = '#dc2626';
+const slate = '#cbd5e1';
+const dark = '#0f172a';
 
 const generateData = () => {
-    const data = [];
-    let failed = false;
+  const data = [];
+  let failed = false;
   
-    for (let i = 0; i < 10; i++) {  
-      failed = Math.random() > 0.8 || failed;  
-      const start = i > 0 ? data[i-1].y[1] : 0; 
-      const point = {
-        x: 'Task ' + i,
-        y: [start, Math.random() > 0.5 ? start+10*(Math.random()+1) : 0.5+start+Math.random()],
-        fillColor: failed ? '#FF0000' : '#20AF2E'
-      }
-      data.push(point)
+  for (let i = 0; i < 10; i++) {  
+    failed = Math.random() > 0.8 || failed;  
+    const start = i > 0 ? data[i-1].y[1] : 0; 
+    const point = {
+      x: 'Task ' + i,
+      y: [start, Math.random() > 0.5 ? start+10*(Math.random()+1) : 0.5+start+Math.random()],
+      fillColor: failed ? slate : indigo
     }
+    data.push(point)
+  }
     
-    return data;  
+  return data;  
+ }
+
+const toMicro = (time) => {
+  const minutes = parseFloat(time.split(':')[1])*60;
+  const sec = parseFloat(time.split(':')[2]);
+  return (minutes+sec)*1000000;
+}
+
+const durationMicro = (duration) => {
+  return parseFloat((duration.split('T')[1]).split('S')[0])*1000000;
+}
+
+const getEndTime = (data) => {
+  var max = -1;
+  for (const action in data) {
+    if (data[action]['y'][1] > max) {
+      max = data[action]['y'][1];
+    }
+  }
+  return max;
+}
+
+const parseStateFileSerie = () => {
+  const stateFile = require('../assets/config/state/compute-distances.1.1.json');
+  const runStartTime = toMicro(stateFile.runStartTime);
+  const attempStartTime = toMicro(stateFile.attemptStartTime);
+  var actions = [];
+
+  for (const action in stateFile.actionsState) {
+    const state = stateFile.actionsState[action].state;
+    const start = toMicro(stateFile.actionsState[action].startTstmp);
+    const duration = durationMicro(stateFile.actionsState[action].duration);
+
+    actions.push({
+          x: action,
+          y: [start-runStartTime, start + duration-runStartTime],
+          fillColor: (state === 'SUCCEEDED') ? indigo : red
+    });
   }
 
-const parseData = () => {
-  return null;
+  var series =  [
+    {
+      name: 'Run',
+      data: [
+        {
+          x: 'Attempt ' + stateFile.attemptId + ' runtime',
+          y: [attempStartTime-runStartTime, getEndTime(actions)],
+          fillColor: '#94a3b8'
+        }
+      ]
+    },
+    {
+      name: 'Actions',
+      data: actions
+    }
+  ]
+  
+  return series;
 }
+
+const opt1 = {
+  chart: {
+    type: 'rangeBar',
+    id: 'chart2',
+    toolbar: {
+      autoSelected: 'pan',
+      show: true
+    },
+    
+  },
+  grid: {
+    xaxis: {
+      lines: {
+        show: false
+      }
+    },
+    yaxis: {
+      lines: {
+          show: true
+      }
+    }
+  },
+  plotOptions: {
+    bar: {
+      horizontal: true,
+      borderRadius: 1,
+      barHeight: '70%',
+      rangeBarGroupRows: true,
+      dataLabels: {
+        position: 'center'
+      }
+    },
+  },
+  xaxis: {
+    type: 'numeric',
+  },
+  dataLabels: {
+    enabled: true,
+    style: {
+      fontWeight: 'light',
+      colors: [dark]
+    },
+    formatter: function(value, { seriesIndex, dataPointIndex, w }) {
+      return parseFloat(parseInt(w.config.series[seriesIndex].data[dataPointIndex].y[1]) - parseInt(w.config.series[seriesIndex].data[dataPointIndex].y[0]))/1000000 + ' s';
+    },
+    background: {
+      enabled: true,
+      foreColor: '#fff',
+      padding: 2,
+      borderRadius: 2,
+      borderWidth: 1,
+      borderColor: slate,
+      opacity: 0.1,
+      dropShadow: {
+        enabled: false,
+        top: 1,
+        left: 1,
+        blur: 1,
+        color: '#000',
+        opacity: 0.45
+      }
+    }
+  },
+  colors: [
+    '#64748b', indigo
+  ]
+};
+
+const opt2 = {
+  chart: {
+    type: 'rangeBar',
+    id: 'chart1',
+    toolbar: {
+      autoSelected: 'selection',
+      show: false
+    },
+    brush:{
+      target: 'chart2',
+      enabled: false
+    },
+    selection: {
+      enabled: true,
+      xaxis: {
+        min: 70000,
+        max: 1100000
+      },
+      fill: {
+        color: '#ccc',
+        opacity: 0.2
+      },
+      stroke: {
+        color: '#0D47A1',
+      }
+    },
+  },
+  
+  grid: {
+    xaxis: {
+      lines: {
+        show: false
+      }
+    },
+    yaxis: {
+      lines: {
+          show: true
+      }
+    }
+  },
+  plotOptions: {
+    bar: {
+      horizontal: true,
+      barHeight: '50%',
+      rangeBarGroupRows: true,
+      dataLabels: {
+        position: 'center'
+      }
+    },
+  },
+  xaxis: {
+    labels: {
+      show: false
+    }
+  },
+  yaxis: {
+    labels: {
+      show: false
+    }
+  },
+  legend: {
+    show: false
+  },
+  colors: [
+    slate, indigo
+  ]
+};
 
 const Timeline = () => {
+    const _series = parseStateFileSerie();
     const state = {      
-        series: [
-          {
-            data: generateData()
-          }
-        ],
-        options: {
-          grid: {
-            xaxis: {
-              lines: {
-                show: false
-              }
-            },
-            yaxis: {
-              lines: {
-                  show: true
-              }
-            }
-          },
-          chart: {
-            type: 'rangeBar'
-          },
-          plotOptions: {
-            bar: {
-              horizontal: true,
-              dataLabels: {
-                hideOverflowingLabels: false
-              }
-            },
-          },
-          xaxis: {
-            type: 'numeric'
-          }
-        },
-      };
+        series: _series,
+        options: opt1
+      }
+
+    const state2 = {
+      series: _series,
+      options: opt2
+    }
 
     return ( 
-        <Chart 
-            options={state.options}
-            series={state.series}
-            type="rangeBar"
-            height="400"
-          />
-     );
-}
+      <>
+        <div>
+          <ReactApexChart 
+            options={state.options} 
+            series={state.series} 
+            type="rangeBar" 
+            height="300"/>
+        </div>
+        <div>
+          <ReactApexChart 
+            options={state2.options} 
+            series={state2.series} 
+            type="rangeBar" 
+            height="100"/>
+        </div>
+      </>
+      );
+  }
  
 export default Timeline;
